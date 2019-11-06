26ab042a0d97ab7894643ad0f8a81ce65b3062db
marianwhat <maria_zapata@hotmail.it>
2019-11-06 17:20:22 -0500

    Updates

---
+++ precache-manifest.51ecaebea82041f9d6a62e8c63efa91b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4eb7a54d9be4db149b8839fd530547",
+    "url": "/Web-App-Reserva-Anuncios/index.html"
+  },
+  {
+    "revision": "be4309a9b073cdff70b1",
+    "url": "/Web-App-Reserva-Anuncios/static/css/main.2dc64da8.chunk.css"
+  },
+  {
+    "revision": "d51921bdb148dc8d02f7",
+    "url": "/Web-App-Reserva-Anuncios/static/js/2.f9ce4f0e.chunk.js"
+  },
+  {
+    "revision": "be4309a9b073cdff70b1",
+    "url": "/Web-App-Reserva-Anuncios/static/js/main.d73df4c8.chunk.js"
+  },
+  {
+    "revision": "02ad4c60681223627a82",
+    "url": "/Web-App-Reserva-Anuncios/static/js/runtime-main.5598b088.js"
+  },
+  {
+    "revision": "469cfe3616f6507a25fc4675db42e9f7",
+    "url": "/Web-App-Reserva-Anuncios/static/media/logo.469cfe36.svg"
+  }
+]);
